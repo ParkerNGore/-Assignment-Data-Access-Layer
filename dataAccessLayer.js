48373d75519a52a9054f736b21aff99e234933d8
parkerngore <parkerngore@gmail.com>
2020-08-25 20:39:55 -0600

    Character API for Final Project, Draft 1

---
+++ dataAccessLayer.js
@@ -0,0 +1,161 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
+const url = process.env.MONGODB_URL;
+const databaseName = process.env.MONGODB_DATABASE;
+
+// Validation for .env file.
+if (!url) {
+  console.log(
+    "DATABASE URL NOT SPECIFIED PLEASE ADD THE URL TO YOUR ENV FILE."
+  );
+  return;
+}
+
+if (!databaseName) {
+  console.log(
+    "DATABASE DATABASE NAME NOT SPECIFIED. PLEASE ADD THE MONGODB_DATABASE TO YOUR ENV FILE."
+  );
+  return;
+}
+
+console.log(url);
+console.log(databaseName);
+
+const collectionName = "characters";
+const settings = {
+  seeUnifiedTopology: true,
+};
+
+let databaseClient;
+let characterCollection;
+
+const connect = function () {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(url, settings, (error, client) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+      databaseClient = client.db(databaseName);
+      characterCollection = databaseClient.collection(collectionName);
+      console.log("SUCCESSFULLY CONNECTED TO DATABASE");
+      resolve();
+    });
+  });
+};
+
+const insertOne = function (character) {
+  return new Promise((resolve, reject) => {
+    characterCollection.insertOne(character, (error, result) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+      console.log("SUCCESSFULLY INSTERED A NEW DOCUMENT!");
+      resolve();
+    });
+  });
+};
+
+const findAll = function () {
+  const query = {};
+
+  return new Promise((resolve, reject) => {
+    characterCollection.find(query).toArray((error, documents) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+
+      console.log(`SUCCESSFULLY FOUND ${documents.length} DOCUMENTS`);
+      resolve(documents);
+    });
+  });
+};
+
+const findOne = function (query) {
+  return new Promise((resolve, reject) => {
+    characterCollection.find(query).toArray((error, documents) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+
+      if (documents.length > 0) {
+        console.log("SUCCESSFULLY FOUND DOCUMENT!");
+        const document = documents[0];
+        resolve(document);
+      } else {
+        reject("No document found!");
+      }
+    });
+  });
+};
+
+const updateOne = function (query, newCharacter) {
+  const newCharacterQuery = {};
+
+  if (newCharacter.name) {
+    newCharacterQuery.name = newCharacter.name;
+  }
+
+  if (newCharacter.level) {
+    newCharacterQuery.level = newCharacter.level;
+  }
+
+  if (newCharacter.race) {
+    newCharacterQuery.race = newCharacter.race;
+  }
+
+  if (newCharacter.class) {
+    newCharacterQuery.class = newCharacter.class;
+  }
+
+  return new Promise((resolve, reject) => {
+    characterCollection.updateOne(
+      query,
+      { $set: newCharacterQuery },
+      (error, result) => {
+        if (error) {
+          console.log(error);
+          reject;
+          return;
+        } else if (result.modifiedCount === 0) {
+          console.log("No Document Found");
+          reject("No Document Found");
+          return;
+        }
+
+        console.log("SUCCESSFULLY UPDATED DOCUMENT!");
+        resolve();
+      }
+    );
+  });
+};
+
+const deleteOne = function (query) {
+  return new Promise((resolve, reject) => {
+    characterCollection.deleteOne(query, (error, result) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      } else if (result.deletedCount === 0) {
+        console.log("No Document Found");
+        reject("No Document Found");
+        return;
+      }
+
+      console.log("SUCCESSFULLY DELETED DOCUMENT!");
+      resolve();
+    });
+  });
+};
+
+module.exports = { connect, insertOne, findAll, findOne, updateOne, deleteOne };
---
+++ server.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dataAccessLayer = require("./dataAccessLayer");
+const cors = require("cors");
+const { ObjectId, ObjectID } = require("mongodb");
+
+dataAccessLayer.connect();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get("/api/characters", async (request, response) => {
+  const characters = await dataAccessLayer.findAll();
+
+  response.send(characters);
+});
+
+app.get("api/characters/:id", async (request, response) => {
+  const characterId = request.params.id;
+
+  if (!ObjectID.isValid(characterId)) {
+    response.status(400).send(`CharacterID ${characterId} is incorrect.`);
+    return;
+  }
+
+  const charcterQuery = {
+    _id: new ObjectId(characterId),
+  };
+  let character;
+
+  try {
+    character = await dataAccessLayer.findOne(charcterQuery);
+  } catch (error) {
+    response.status(404).send(`Character with id ${characterId} not found!`);
+    return;
+  }
+  response.send(character);
+});
+
+app.post("/api/characters", async (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.level || !body.class || !body.race) {
+    response
+      .status(400)
+      .send(
+        "Bad Request. Validation Error. Please ensure all parameters are included."
+      );
+    return;
+  }
+
+  if (typeof body.name !== "string") {
+    response.status(400).send("The name parameter must be of type string");
+    return;
+  }
+
+  if (isNaN(Number(body.level)) || body.level <= 0) {
+    response.status(400).send("The level parameter must be of type number");
+    return;
+  }
+
+  if (typeof body.class !== "string") {
+    response
+      .status(400)
+      .send("The class name parameter must be of type string");
+    return;
+  }
+
+  if (typeof body.race !== "string") {
+    response.status(400).send("The race name parameter must be of type string");
+    return;
+  }
+
+  await dataAccessLayer.insertOne(body);
+
+  response.status(201).send();
+});
+
+app.put("/api/characters/:id", async (request, response) => {
+  const characterId = request.params.id;
+  const body = request.body;
+
+  if (!ObjectID.isValid(characterId)) {
+    response.status(400).send(`CharacterID: ${characterId} is incorrect.`);
+    return;
+  }
+
+  if (body.name && typeof body.name !== "string") {
+    response.status(400).send("The name parametr must be of type string");
+  }
+  if ((body.level && isNaN(Number(body.level))) || body.level <= 0) {
+    response.status(400).send("The level parameter must be of type number");
+  }
+  if (body.class && typeof body.class !== "string") {
+    response.status(400).send("The class parameter must be of type string");
+  }
+  if (body.race && typeof body.race !== "string") {
+    response.status(400).send("The race parameter must be of type string");
+  }
+
+  const characterQuery = {
+    _id: new ObjectID(characterId),
+  };
+  try {
+    await dataAccessLayer.updateOne(characterQuery, body);
+  } catch (error) {
+    response.status(404).send(`Character with id: ${characterId} not found!`);
+    return;
+  }
+
+  response.send();
+});
+
+app.delete("/api/characters/:id", async (request, response) => {
+  const characterId = request.params.id;
+
+  if (!ObjectID.isValid(characterId)) {
+    response.status(400).send(`CharacterID ${characterId} is incorrect.`);
+    return;
+  }
+
+  const characterQuery = {
+    _id: new ObjectId(characterId),
+  };
+  try {
+    await dataAccessLayer.deleteOne(characterQuery);
+  } catch (error) {
+    response.status(404).send(`Character with id ${characterId} not found!`);
+    return;
+  }
+
+  response.send();
+});
+
+const port = process.env.PORT ? process.env.PORT : 3005;
+app.listen(port, () => {
+  console.log("Character Collection API Server Started");
+});
